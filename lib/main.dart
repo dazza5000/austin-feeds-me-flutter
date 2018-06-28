dc2b412f81eb409afdce6b31e1cac94816a1e2d5
Darran Kelinske <darran.kelinske@gmail.com>
2018-06-27 21:23:26 -0500

    minor tweaks to flutter

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,6 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Austin Feeds Me',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(title: 'Austin Feeds Me'),
@@ -29,15 +21,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -45,11 +28,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  List<String> allData = [];
+
   @override
   void initState() {
     super.initState();
-
-    List<myData> allData = [];
 
     DatabaseReference ref = FirebaseDatabase.instance.reference();
     ref.child('events').once().then((DataSnapshot snap) {
@@ -57,35 +41,38 @@ class _MyHomePageState extends State<MyHomePage> {
       var data = snap.value;
       for (var key in keys) {
         debugPrint(data[key]['name']);
-      };
+        allData.add(data[key]['name']);
+      }
+      setState(() {
+        print(allData);
+      });
     });
   }
-}
 
-@override
-Widget build(BuildContext context) {
-  return new Scaffold(
-    appBar: new AppBar(
-      title: new Text(widget.title),
-    ),
-    body: new Container(
-        child: allData.length == 0
-            ? new Text(' No Data is Available')
-            : new ListView.builder(
-          itemCount: allData.length,
-          itemBuilder: (_, index) {
-            return _buildRow(allData[index].name);
-          },
-        )),
-  );
-}
 
-Widget _buildRow(String rowTitle) {
-  return new ListTile(
-    title: new Text(
-      rowTitle,
-      style: const TextStyle(fontSize: 18.0),
-    ),
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Container(
+          child: allData.length == 0
+              ? new Text(' No Data is Available')
+              : new ListView.builder(
+            itemCount: allData.length,
+            itemBuilder: (_, index) {
+              return _buildRow(allData[index]);
+            },
+          )),
+    );
+  }
+
+  Widget _buildRow(String rowTitle) {
+    return new Row(
+        children: <Widget>[
+    new Expanded(
+    child: new Text(rowTitle),
+    ),]);
+  }
 }
