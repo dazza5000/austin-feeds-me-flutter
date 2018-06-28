ab271ec0cb9fed95549ec15a379146f5dc9e5586
Darran Kelinske <dkelinske@c02t9b70gtfm.wvrgroup.internal>
2018-06-28 15:34:17 -0500

    add tap listener to card to get more details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:austin_feeds_me/model/austin_feeds_me_event.dart';
 
 void main() => runApp(new MyApp());
 
@@ -29,7 +31,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  List<String> allData = [];
+  List<AustinFeedsMeEvent> events = [];
+  int currentTab = 0; // Index of currently opened tab.
+  List<Widget> pages; // List of all pages that can be opened from our BottomNavigationBar.
+  // Index 0 represents the page for the 0th tab, index 1 represents the page for the 1st tab etc...
+  Widget currentPage; // Page that is open at the moment.
 
   @override
   void initState() {
@@ -41,11 +47,18 @@ class _MyHomePageState extends State<MyHomePage> {
       var data = snap.value;
       for (var key in keys) {
         if (data[key]['food']) {
-          allData.add(data[key]['name'] + DateTime.fromMillisecondsSinceEpoch(data[key]['time']).toString());
+          events.add(new AustinFeedsMeEvent(
+              name: data[key]['name'],
+              time: data[key]['time'],
+              description: data[key]['description'],
+          url: data[key]['event_url']));
         }
       }
       setState(() {
-        print("allData length is: "+allData.length.toString());
+        events.sort((a, b) {
+          return a.time.compareTo(b.time);
+        });
+        print("allData length is: "+events.length.toString());
       });
     });
   }
@@ -53,27 +66,71 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final BottomNavigationBar navBar = new BottomNavigationBar(
+      currentIndex: currentTab, // Our currentIndex will be the currentTab value. So we need to update this whenever we tab on a new page!
+      onTap: (int numTab) { // numTab will be the index of the tab that is pressed.
+        setState(() { // Setting the state so we can show our new page.
+          print("Current tab: " + numTab.toString()); // Printing for debugging reasons.
+          currentTab = numTab; // Updating our currentTab with the tab that is pressed [See 43].
+          currentPage = pages[numTab]; // Updating the page that we'd like to show to the user.
+        });
+      },
+      items: <BottomNavigationBarItem>[ // Visuals, see docs for more information: https://docs.flutter.io/flutter/material/BottomNavigationBar-class.html
+        new BottomNavigationBarItem( //numTab 0
+            icon: new Icon(Icons.list),
+            title: new Text("List")
+        ),
+        new BottomNavigationBarItem( //numTab 1
+            icon: new Icon(Icons.map),
+            title: new Text("Map")
+        ),
+      ],
+    );
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
       body: new Container(
-          child: allData.length == 0
+          child: events.length == 0
               ? new Text(' No Data is Available')
               : new ListView.builder(
-            itemCount: allData.length,
+            padding: const EdgeInsets.all(16.0),
+            itemCount: events.length,
             itemBuilder: (_, index) {
-              return _buildRow(allData[index]);
+              return _buildRow(events[index]);
             },
           )),
+      bottomNavigationBar: new BottomAppBar(
+        child: navBar,
+      ),
     );
   }
 
-  Widget _buildRow(String rowTitle) {
-    return new Row(
+  Widget _buildRow(AustinFeedsMeEvent event) {
+    return new GestureDetector(
+        child: new Card(
+        child: new Row(
+          mainAxisSize: MainAxisSize.min,
         children: <Widget>[
     new Expanded(
-    child: new Text(rowTitle),
-    ),]);
+    child: new Text(event.name),
+    ),
+    new Text(DateTime.fromMillisecondsSinceEpoch(event.time).toString()),
+        ])
+    ),
+    onTap: () => eventTapped(event));
+  }
+
+  eventTapped(AustinFeedsMeEvent event) {
+    _launchURL(event.url);
+  }
+}
+
+_launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
--- lib/model/austin_feeds_me_event.dart
+++ lib/model/austin_feeds_me_event.dart
@@ -1,13 +1,15 @@
 import 'package:meta/meta.dart';
 
-class Event {
-  Event({
+class AustinFeedsMeEvent {
+  AustinFeedsMeEvent({
     @required this.name,
     @required this.time,
     this.description,
+    this.url
   });
 
   final String name;
   final int time;
   final String description;
+  final String url;
 }
