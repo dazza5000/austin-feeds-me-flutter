9c134b2a46e68762eebac91b473015eae716fff7
Darran Kelinske <darran.kelinske@gmail.com>
2018-06-28 20:27:10 -0500

    format dates

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:austin_feeds_me/model/austin_feeds_me_event.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -30,19 +30,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List<AustinFeedsMeEvent> events = [];
-  int currentTab = 0; // Index of currently opened tab.
-  List<Widget> pages; // List of all pages that can be opened from our BottomNavigationBar.
-  // Index 0 represents the page for the 0th tab, index 1 represents the page for the 1st tab etc...
-  Widget currentPage; // Page that is open at the moment.
+  int currentTab = 0;
+  List<Widget> pages;
+  Widget currentPage;
 
   @override
   void initState() {
     super.initState();
 
     DatabaseReference ref = FirebaseDatabase.instance.reference();
-    ref.child('events').orderByChild('time').startAt(new DateTime.now().millisecondsSinceEpoch).once().then((DataSnapshot snap) {
+    ref
+        .child('events')
+        .orderByChild('time')
+        .startAt(new DateTime.now().millisecondsSinceEpoch)
+        .once()
+        .then((DataSnapshot snap) {
       var keys = snap.value.keys;
       var data = snap.value;
       for (var key in keys) {
@@ -51,39 +54,40 @@ class _MyHomePageState extends State<MyHomePage> {
               name: data[key]['name'],
               time: data[key]['time'],
               description: data[key]['description'],
-          url: data[key]['event_url']));
+              url: data[key]['event_url']));
         }
       }
       setState(() {
         events.sort((a, b) {
           return a.time.compareTo(b.time);
         });
-        print("allData length is: "+events.length.toString());
+        print("allData length is: " + events.length.toString());
       });
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     final BottomNavigationBar navBar = new BottomNavigationBar(
-      currentIndex: currentTab, // Our currentIndex will be the currentTab value. So we need to update this whenever we tab on a new page!
-      onTap: (int numTab) { // numTab will be the index of the tab that is pressed.
-        setState(() { // Setting the state so we can show our new page.
-          print("Current tab: " + numTab.toString()); // Printing for debugging reasons.
-          currentTab = numTab; // Updating our currentTab with the tab that is pressed [See 43].
-          currentPage = pages[numTab]; // Updating the page that we'd like to show to the user.
+      currentIndex: currentTab,
+      // Our currentIndex will be the currentTab value. So we need to update this whenever we tab on a new page!
+      onTap: (int numTab) {
+        // numTab will be the index of the tab that is pressed.
+        setState(() {
+          // Setting the state so we can show our new page.
+          print("Current tab: " +
+              numTab.toString()); // Printing for debugging reasons.
+          currentTab =
+              numTab; // Updating our currentTab with the tab that is pressed [See 43].
+          currentPage = pages[
+              numTab]; // Updating the page that we'd like to show to the user.
         });
       },
-      items: <BottomNavigationBarItem>[ // Visuals, see docs for more information: https://docs.flutter.io/flutter/material/BottomNavigationBar-class.html
-        new BottomNavigationBarItem( //numTab 0
-            icon: new Icon(Icons.list),
-            title: new Text("List")
-        ),
-        new BottomNavigationBarItem( //numTab 1
-            icon: new Icon(Icons.map),
-            title: new Text("Map")
-        ),
+      items: <BottomNavigationBarItem>[
+        new BottomNavigationBarItem(
+            icon: new Icon(Icons.list), title: new Text("List")),
+        new BottomNavigationBarItem(
+            icon: new Icon(Icons.map), title: new Text("Map")),
       ],
     );
 
@@ -93,14 +97,14 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: new Container(
           child: events.length == 0
-              ? new Text(' No Data is Available')
+              ? new Center(child: new Text('Loading...'))
               : new ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            itemCount: events.length,
-            itemBuilder: (_, index) {
-              return _buildRow(events[index]);
-            },
-          )),
+                  padding: const EdgeInsets.all(16.0),
+                  itemCount: events.length,
+                  itemBuilder: (_, index) {
+                    return _buildRow(events[index]);
+                  },
+                )),
       bottomNavigationBar: new BottomAppBar(
         child: navBar,
       ),
@@ -108,18 +112,26 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildRow(AustinFeedsMeEvent event) {
+    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(event.time);
+    String date = new DateFormat.yMMMMEEEEd().format(dateTime);
+
+    String time = new DateFormat.jm().format(dateTime);
+
     return new GestureDetector(
         child: new Card(
-        child: new Row(
-          mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-    new Expanded(
-    child: new Text(event.name),
-    ),
-    new Text(DateTime.fromMillisecondsSinceEpoch(event.time).toString()),
-        ])
-    ),
-    onTap: () => eventTapped(event));
+            child: new Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          new Expanded(
+            child: new Text(event.name),
+          ),
+          new Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              new Text(date),
+              new Text(time),
+            ],
+          ),
+        ])),
+        onTap: () => eventTapped(event));
   }
 
   eventTapped(AustinFeedsMeEvent event) {
